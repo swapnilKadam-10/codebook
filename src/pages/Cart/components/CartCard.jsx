9aa0cf8df11cce3e361e632842a482f27bcd8ee5
kadamswapnil4259@gmail.com <kadamswapnil4259@gmail.com>
2024-06-18 20:24:07 +0530

    .

--- src/pages/Cart/components/CartCard.jsx
+++ src/pages/Cart/components/CartCard.jsx
@@ -1,26 +1,30 @@
-import { Link } from 'react-router-dom';
-import { useCart } from '../../../context';
+import { Link } from "react-router-dom";
+import { useCart } from "../../../context";
 
-export const CartCard = ({product}) => {
-
+export const CartCard = ({ product }) => {
   const { id, name, price, poster } = product;
   const { removeFromCart } = useCart();
   return (
     <div className="flex flex-wrap justify-between border-b dark:border-slate-700 max-w-4xl m-auto p-2 mb-5 ">
       <div className="flex">
+        <Link to={`/products/${id}`}>
+          <img className="w-32 rounded" src={poster} alt="" />
+        </Link>
+        <div className="">
           <Link to={`/products/${id}`}>
-            <img className="w-32 rounded" src={poster} alt="" />
+            <p className="text-lg ml-2 dark:text-slate-200">{name}</p>
           </Link>
-          <div className="">
-          <Link to={`/products/${id}`}>
-              <p className="text-lg ml-2 dark:text-slate-200">{name}</p>
-            </Link>            
-            <button onClick={() => removeFromCart(product)} className="text-base ml-2 text-red-700">Remove</button>
-          </div>
+          <button
+            onClick={() => removeFromCart(product)}
+            className="text-base ml-2 text-red-700"
+          >
+            Remove
+          </button>
+        </div>
       </div>
       <div className="text-lg m-2 dark:text-slate-200">
         <span>₹{price}</span>
       </div>
     </div>
-  )
-}
+  );
+};
